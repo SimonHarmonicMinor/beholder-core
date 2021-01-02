17ffa87cbb557100ff38f2e18d5763ebd97dd3ac
Semyon Kirekov <semyon@kirekov.com>
2021-01-02 20:25:40 +0300

    [#13] - Changed the signature of BeholderProcess.verifyProject. Now it returns java.util.List instead of com.kirekov.collection.immutable.ImmutableList

--- src/main/java/com/kirekov/beholder/core/process/BeholderProcess.java
+++ src/main/java/com/kirekov/beholder/core/process/BeholderProcess.java
@@ -1,9 +1,10 @@
 package com.kirekov.beholder.core.process;
 
+import com.kirekov.beholder.core.exception.BeholderProcessException;
 import com.kirekov.beholder.core.verifier.VerifyingError;
-import com.github.simonharmonicminor.juu.collection.immutable.ImmutableList;
 
 import java.io.File;
+import java.util.List;
 
 /**
  * Defines main process that scans the project and returns found errors
@@ -14,6 +15,7 @@ public interface BeholderProcess {
      *
      * @param pathToMainPackage path to package that should be verified
      * @return found errors
+     * @throws BeholderProcessException if cannot read sources from the given file
      */
-    ImmutableList<VerifyingError> verifyProject(File pathToMainPackage);
+    List<VerifyingError> verifyProject(File pathToMainPackage);
 }
